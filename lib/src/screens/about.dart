4ba5648cae7d2530857f2ce91b51f5fd16aceb05
sappybp <49954969+sappybp@users.noreply.github.com>
2025-01-02 22:18:15 +0900

    addPrivacyPolicyUrl

--- lib/src/screens/about.dart
+++ lib/src/screens/about.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import 'package:noveru/src/screens/privacyPolicy.dart';
 import 'package:noveru/src/screens/howToUse.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
+
+  Future onLaunchUrlPrivatePolicy () async {
+    final url = Uri.parse('https://sappy-info.jimdofree.com/%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC/');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+      );
+    } else {
+      print('cant open url');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +35,7 @@ class AboutScreen extends StatelessWidget {
               fixedSize: const Size(double.infinity, 50),
             ),
             onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const PrivacyPolicy()
-                ),
-              );
+              onLaunchUrlPrivatePolicy();
             },
             child: const Text("プライバシーポリシー",
               style: TextStyle(fontSize: 18,
