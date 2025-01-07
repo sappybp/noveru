18011741f2536fcabe1970fec1d224d1b97e6498
sappybp <sappybp@gmail.com>
2025-01-07 09:53:26 +0900

    fixIosValue and addTermsOfService

--- lib/src/screens/about.dart
+++ lib/src/screens/about.dart
@@ -6,8 +6,20 @@ import 'package:noveru/src/screens/howToUse.dart';
 class AboutScreen extends StatelessWidget {
   const AboutScreen({super.key});
 
-  Future onLaunchUrlPrivatePolicy () async {
+  Future onLaunchUrlPrivatePolicy() async {
     final url = Uri.parse('https://sappy-info.jimdofree.com/%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC/');
+    if (await canLaunchUrl(url)) {
+      await launchUrl(
+        url,
+        mode: LaunchMode.externalApplication,
+      );
+    } else {
+      print('cant open url');
+    }
+  }
+
+  Future onLaunchUrlTermsOfService() async {
+    final url = Uri.parse('https://sappy-info.jimdofree.com/%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%84/');
     if (await canLaunchUrl(url)) {
       await launchUrl(
         url,
@@ -31,7 +43,6 @@ class AboutScreen extends StatelessWidget {
           ),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
-              // 幅150、高さ50のボタン
               fixedSize: const Size(double.infinity, 50),
             ),
             onPressed: () {
@@ -48,7 +59,22 @@ class AboutScreen extends StatelessWidget {
           ),
           ElevatedButton(
             style: ElevatedButton.styleFrom(
-              // 幅150、高さ50のボタン
+              fixedSize: const Size(double.infinity, 50),
+            ),
+            onPressed: () {
+              onLaunchUrlTermsOfService();
+            },
+            child: const Text("利用規約",
+              style: TextStyle(fontSize: 18,
+                  fontWeight: FontWeight.bold
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
               fixedSize: const Size(double.infinity, 50),
             ),
             onPressed: () {
